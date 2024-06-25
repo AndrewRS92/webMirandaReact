b1522effd58c35d43c5163170cac14526767a287
AndrewRS92 <andrew.rojas.saldias@gmail.com>
2024-06-25 10:10:32 +0200

    first commit

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,41 @@
+import {useState} from "react"
+
+
+
+export function LoginForm(){
+    
+    const [user, setUSer] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState(false)
+
+    const handleSubmit =(e)=>{
+        e.preventDefault()
+        if (user === "" || password ===""){
+            setError(true)
+            return
+        }
+    }
+    
+    
+    return <section>
+        <h1>Login</h1>
+
+    <form
+        onSubmit={handleSubmit}
+    >
+        <input type="text" 
+        value={user}
+        onChange={e=> setUSer(e.target.value)}
+        />
+        <input type="password"
+        value={password}
+        onChange={e=> setPassword(e.target.value)}
+        />
+        <button>Login</button>
+
+
+
+    </form>
+
+    </section>
+}

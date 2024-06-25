916cbe1812cb992fd0025d48c2b55db9ff357e5e
AndrewRS92 <andrew.rojas.saldias@gmail.com>
2024-06-25 23:25:59 +0200

    añadiendo estilos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,11 @@ import '../styles/Header.css'
 import Menu from './Menu'
 import { useLocation } from 'react-router-dom';
 import React, { useState } from 'react';
+import { IoIosSearch } from "react-icons/io";
+import { FaRegEnvelope } from "react-icons/fa6";
+import { GoBell } from "react-icons/go";
+import { BiCommentDetail } from "react-icons/bi";
+import { HiMenuAlt2 } from "react-icons/hi";
 
 const Header = () => {
     const location = useLocation();
@@ -18,12 +23,25 @@ const Header = () => {
       <div className="header">
         <nav>{getPageName(location.pathname)}</nav>
     <div className="dropdown">
-        <button  onClick={toggleMenu}>Menú</button>
+        <button onClick={toggleMenu}className="menu__icon">{<HiMenuAlt2 />}</button>
         {isMenuVisible && (<Menu />)}
     </div>
-    <div>
-    </div>
+
+    <div className="search"> 
+      <div className="searchWrapper">
+                <input type="text" className="searchInput" />
+                <IoIosSearch className="search__Icon" />
+            </div>
+            </div>
+    <div className="icons">  
+            
+            <FaRegEnvelope className="icon" />
+            <GoBell className="icon" />
+            <BiCommentDetail className="icon" /> 
+            </div>
+
       </div>
+      
     );
   };
 
